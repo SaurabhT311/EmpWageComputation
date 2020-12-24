b0f2873255e23112cd6c9122c9299439afb83556
SaurabhT311 <tripathisaurabh.311@gmail.com>
2020-12-25 02:23:21 +0530

    Add: Added switch case to calculate employee wage

--- empWage.java
+++ empWage.java
@@ -2,18 +2,27 @@ class empWage
 {
         public static void main(String[] args)
         {
-                System.out.println("Welcome to the Employee  Wage Computation");
-		int res=(int)((Math.random()*10)%2);
-		int isPresent=0;
-		if(res==isPresent)
+		int wage_per_hr=20;
+		int fullTime=1;
+		int empHrs=0;
+      		int partTime=2;
+		int salary=0;
+		System.out.println("Welcome to the Employee Wage Computation");
+		int empCheck=(int)(Math.floor(Math.random()*10)%3);
+		switch(empCheck)
 		{
-			System.out.println("Employee is present");
-	        }
-		else{
-			System.out.println("Employee is Absent");
-		}
+			case 1:
+				empHrs=8;
+				break;
+
+			case 2:
+				empHrs=4;
+				break;
+
+			default:
+				empHrs=0;
 	}
+	salary=empHrs*wage_per_hr;
+	System.out.println("Employee Salary: "+ salary);
+  }
 }
-
-
-
