c84a3419c24dd1a42ffbc1bd9c8031c587dfa1d2
SaurabhT311 <tripathisaurabh.311@gmail.com>
2020-12-25 02:39:33 +0530

    Add: Computing Employee Wages for a month

--- empWage.java
+++ empWage.java
@@ -2,18 +2,31 @@ class empWage
 {
         public static void main(String[] args)
         {
-                System.out.println("Welcome to the Employee  Wage Computation");
-		int res=(int)((Math.random()*10)%2);
-		int isPresent=0;
-		if(res==isPresent)
+		int wage_per_hr=20;
+		int fullTime=1;
+		int empHrs=0;
+      		int partTime=2;
+		int salary=0;
+		int workday_in_month=20;
+		int TotalSalary=0;
+		for(int i=1;i<=workday_in_month;i++)
 		{
-			System.out.println("Employee is present");
-	        }
-		else{
-			System.out.println("Employee is Absent");
+			int empCheck=(int)(Math.floor(Math.random()*10)%3);
+			switch(empCheck)
+			{
+				case 1:
+					empHrs=8;
+					break;
+				case 2:
+					empHrs=4;
+					break;
+				default:
+					empHrs=0;
+					break;
+			}
+			salary=empHrs*wage_per_hr;
+			TotalSalary= salary+ TotalSalary;
 		}
-	}
+		System.out.println("Employee Salary in a month: "+ TotalSalary);
+  }
 }
-
-
-
