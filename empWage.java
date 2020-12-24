539a061068b263e72d8b624aeb5a520009020084
SaurabhT311 <tripathisaurabh.311@gmail.com>
2020-12-25 01:38:40 +0530

    Add: Added code to check employee daily wage

--- empWage.java
+++ empWage.java
@@ -2,9 +2,21 @@ class empWage
 {
         public static void main(String[] args)
         {
-                System.out.println("Welcome to the Employee  Wage Computation");
-        }
-}
-
-
+		int wage_per_hr=20;
+		int fulltime=8;
+		int empHrs=0;
+      		int isPresent=1;
+		System.out.println("Welcome to the Employee Wage Computation");
+		int empCheck=(int)(Math.floor(Math.random()*10)%2);
+		if(empCheck==isPresent)
+		{
+			System.out.println("Employee is present");
+			int dailyWage=fulltime*wage_per_hr;
+			System.out.println("Employee daily wage is :"+dailyWage);
+	        }
+		else{
+			System.out.println("Employee is Absent");
+		}
+	}
+  }
 
