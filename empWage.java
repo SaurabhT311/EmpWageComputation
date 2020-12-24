5f4954cf1cfd75dd59650cdbb1e2c172e7e02e29
SaurabhT311 <38470650+SaurabhT311@users.noreply.github.com>
2020-12-25 03:19:05 +0530

    Update empWage.java

--- empWage.java
+++ empWage.java
@@ -1,19 +1,36 @@
 class empWage
 {
+	public static final int Max_working_Hrs=100;
+	public static final int workday_in_months=20;
+
         public static void main(String[] args)
         {
-                System.out.println("Welcome to the Employee  Wage Computation");
-		int res=(int)((Math.random()*10)%2);
-		int isPresent=0;
-		if(res==isPresent)
+		int fullTime=1;
+		int partTime=2;
+		int wage_per_hr=20;
+		int empHrs=0;
+		int totalempHrs=0;
+		int totalworkingdays=0;
+		System.out.println("Welcome to Employee Wage Computation Program in Main branch");
+		while(totalempHrs<=Max_working_Hrs && totalworkingdays<=workday_in_months)
 		{
-			System.out.println("Employee is present");
-	        }
-		else{
-			System.out.println("Employee is Absent");
+			int empCheck=(int)(Math.floor(Math.random()*10)%3);
+			switch(empCheck)
+			{
+				case 1:
+					empHrs=8;
+					break;
+				case 2:
+					empHrs=4;
+					break;
+				default:
+					empHrs=0;
+					break;
+			}
+			totalempHrs=totalempHrs+empHrs;
+			totalworkingdays++;
 		}
+			int TotalSalary= totalempHrs*wage_per_hr;
+			System.out.print("Total Employee Salary is: "+ TotalSalary);
 	}
 }
-
-
-
