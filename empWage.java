42cdac2bb2ccc298cb5b4e5d6538eb1fc3b0c426
SaurabhT311 <tripathisaurabh.311@gmail.com>
2020-12-25 01:16:47 +0530

    Added: Code to check if Employee is present or not

--- empWage.java
+++ empWage.java
@@ -1,7 +1,16 @@
-n class empWage
+class empWage
 {
 	public static void main(String[] args)
 	{
 		System.out.println("Welcome to the Employee  Wage Computation");
+		int res=(int)((Math.random()*10)%2);
+		int isPresent=0;
+		if(res==isPresent)
+		{
+			System.out.println("Employee is present");
+	        }
+		else{
+			System.out.println("Employee is Absent");
+		}
 	}
 }
