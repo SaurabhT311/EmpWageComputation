50d149d6f78998ccd332b9c023312c186fb9ec70
SaurabhT311 <tripathisaurabh.311@gmail.com>
2020-12-25 20:48:55 +0530

    Add: Code added to calculate multiple company wage

--- empWage.java
+++ empWage.java
@@ -1,19 +1,46 @@
-class empWage
+class empWageSal
 {
-        public static void main(String[] args)
+	public static final int isFullTime=1;
+	public static final int isPartTime=2;  
+
+        public void empsalary(String company, int wage_per_hr, int workday_in_months, int max_working_hrs)
         {
-                System.out.println("Welcome to the Employee  Wage Computation");
-		int res=(int)((Math.random()*10)%2);
-		int isPresent=0;
-		if(res==isPresent)
+		int empHrs=0;
+		int totalempHrs=0;
+		int totalworkingdays=0;
+
+		//System.out.println("Welcome to Employee Wage Computation Program in Main branch");
+		while(totalempHrs<=max_working_hrs && totalworkingdays<=workday_in_months)
 		{
-			System.out.println("Employee is present");
-	        }
-		else{
-			System.out.println("Employee is Absent");
+			int empCheck=(int)(Math.floor(Math.random()*10)%3);
+			switch(empCheck)
+			{
+				case 1:
+					empHrs=8;
+					break;
+				case 2:
+					empHrs=4;
+					break;
+				default:
+					empHrs=0;
+					break;
+			}
+			totalempHrs=totalempHrs+empHrs;
+			totalworkingdays++;
 		}
+			int TotalSalary= totalempHrs*wage_per_hr;
+			System.out.println("Total Employee Salary is: "+ TotalSalary);
 	}
 }
 
-
-
+public class empWage
+{	
+	public static void main(String[] args)
+	{
+		System.out.println("Welcome To Compute Wage Problrem");
+                empWageSal empWage = new empWageSal();
+                empWage.empsalary("DeShaw", 25, 20, 100);
+		empWage.empsalary("TCS", 50, 20, 100);
+		empWage.empsalary("NetScope",20,15,100);
+        }
+}
