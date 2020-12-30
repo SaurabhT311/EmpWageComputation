bd7b5f130f23f36d2786468f3e429695db9622dc
SaurabhT311 <tripathisaurabh.311@gmail.com>
2020-12-30 13:03:53 +0530

    Refractor:Code added to compute Employee wage using class variable

--- empWage.java
+++ empWage.java
@@ -2,6 +2,8 @@ class empWageSal
 {
 	public static final int isFullTime=1;
 	public static final int isPartTime=2;  
+	public static final int Max_working_Hrs=100;
+        public static final int workday_in_months=20;
 
         public void empsalary(String company, int wage_per_hr, int workday_in_months, int max_working_hrs)
         {
@@ -11,19 +13,6 @@ class empWageSal
 
 		System.out.println("Welcome to Employee Wage Computation Program in Main branch");
 		while(totalempHrs<=max_working_hrs && totalworkingdays<=workday_in_months)
-	public static final int Max_working_Hrs=100;
-	public static final int workday_in_months=20;
-
-        public static void main(String[] args)
-        {
-		int fullTime=1;
-		int partTime=2;
-		int wage_per_hr=20;
-		int empHrs=0;
-		int totalempHrs=0;
-		int totalworkingdays=0;
-		System.out.println("Welcome to Employee Wage Computation Program in Main branch");
-		while(totalempHrs<=Max_working_Hrs && totalworkingdays<=workday_in_months)
 		{
 			int empCheck=(int)(Math.floor(Math.random()*10)%3);
 			switch(empCheck)
