44d6e8b8ec04d3e060a9dac9d4c18bff4523a1a1
SaurabhT311 <tripathisaurabh.311@gmail.com>
2020-12-30 14:11:54 +0530

    Add: COmputing Employee Wage for multiple companies

--- empWage.java
+++ empWage.java
@@ -1,19 +1,20 @@
 class empWage
 {
-	public static final int Max_working_Hrs=100;
-	public static final int workday_in_months=20;
-
-        public static void computeWage(String comapny, int )
+	//instance variables
+	public static final int isFullTime=1;
+	public static final int isPartTime=2;  
+	
+	//fucntion to compute monthly salary
+        public static void computeWage(String company, int wage_per_hr, int workday_in_months, int max_working_hrs)
         {
-		int fullTime=1;
-		int partTime=2;
-		int wage_per_hr=20;
 		int empHrs=0;
 		int totalempHrs=0;
 		int totalworkingdays=0;
-		System.out.println("Welcome to Employee Wage Computation Program in Main branch");
-		while(totalempHrs<=Max_working_Hrs && totalworkingdays<=workday_in_months)
+
+		//loop will run unti it reaches 100 hours or 20 days
+		while(totalempHrs<=max_working_hrs && totalworkingdays<=workday_in_months)
 		{
+			//switch case to check for fulltime or parttime
 			int empCheck=(int)(Math.floor(Math.random()*10)%3);
 			switch(empCheck)
 			{
@@ -27,10 +28,19 @@ class empWage
 					empHrs=0;
 					break;
 			}
+			//adding hours in total hours as totalemp hours is 0
 			totalempHrs=totalempHrs+empHrs;
-			totalworkingdays++;
+			totalworkingdays++; //incrementing while loop.
 		}
-			int TotalSalary= totalempHrs*wage_per_hr;
-			System.out.print("Total Employee Salary is: "+ TotalSalary);
+			int TotalSalary= totalempHrs*wage_per_hr; //Calaculating total salary
+			System.out.println( "Total Employee Salary of " + company +" is " + TotalSalary);// printing total salary
 	}
+	//main method
+	public static void main(String[] args)
+	{
+		System.out.println("Welcome to Employee Wage Computation Program in Main branch");
+                computeWage("DeShaw", 25, 20, 100);//calling funtion and paasing arguments
+		computeWage("TCS", 50, 20, 100);//calling function and passing argument
+		computeWage("NetScope",20,15,100);//doing same like asbove two
+        }
 }
