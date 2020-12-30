97285cb9f3e301d8f3eaa15639a57e4524358432
SaurabhT311 <38470650+SaurabhT311@users.noreply.github.com>
2020-12-30 14:15:58 +0530

    Update empWage.java

--- empWage.java
+++ empWage.java
@@ -1,36 +1,49 @@
 class empWage
 {
-	public static final int Max_working_Hrs=100;
-	public static final int workday_in_months=20;
+        //instance variables
+        public static final int isFullTime=1;
+        public static final int isPartTime=2;
 
+        //fucntion to compute monthly salary
+        public static void computeWage(String company, int wage_per_hr, int workday_in_months, int max_working_hrs)
+        {
+		//local variables
+                int empHrs=0;
+                int totalempHrs=0;
+                int totalworkingdays=0;
+
+                //loop will run untill it reaches 100 hours or 20 days
+                while(totalempHrs<=max_working_hrs && totalworkingdays<=workday_in_months)
+                {
+                        //switch case to check for fulltime or parttime
+                        int empCheck=(int)(Math.floor(Math.random()*10)%3);
+                        switch(empCheck)
+                        {
+                                case 1:
+                                        empHrs=8;
+                                        break;
+                                case 2:
+                                        empHrs=4;
+                                        break;
+                                default:
+                                        empHrs=0;
+                                        break;
+                        }
+                        //adding hours in total hours as totalemp hours is 0
+                        totalempHrs=totalempHrs+empHrs;
+                        totalworkingdays++; //incrementing while loop.
+                }
+                        int TotalSalary= totalempHrs*wage_per_hr; //Calculating total salary
+		
+			//printing totalsalary of different companies employee
+                        System.out.println( "Total Employee Salary of " + company +" is " + TotalSalary);
+        }
+        //main method
         public static void main(String[] args)
         {
-		int fullTime=1;
-		int partTime=2;
-		int wage_per_hr=20;
-		int empHrs=0;
-		int totalempHrs=0;
-		int totalworkingdays=0;
-		System.out.println("Welcome to Employee Wage Computation Program in Main branch");
-		while(totalempHrs<=Max_working_Hrs && totalworkingdays<=workday_in_months)
-		{
-			int empCheck=(int)(Math.floor(Math.random()*10)%3);
-			switch(empCheck)
-			{
-				case 1:
-					empHrs=8;
-					break;
-				case 2:
-					empHrs=4;
-					break;
-				default:
-					empHrs=0;
-					break;
-			}
-			totalempHrs=totalempHrs+empHrs;
-			totalworkingdays++;
-		}
-			int TotalSalary= totalempHrs*wage_per_hr;
-			System.out.print("Total Employee Salary is: "+ TotalSalary);
-	}
+                System.out.println("Welcome to Employee Wage Computation Program in Main branch");
+                computeWage("DeShaw", 25, 20, 100);//calling funtion and passing arguments
+                computeWage("TCS", 50, 20, 100);//calling function and passing argument
+                computeWage("NetScope",20,15,100);//doing same like above two
+        }
 }
