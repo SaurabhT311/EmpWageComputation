208072149149bce9b9968ff844ddc975710629be
SaurabhT311 <38470650+SaurabhT311@users.noreply.github.com>
2020-12-31 21:38:19 +0530

    Refractor: Ability to manage Employee Wage of multiple companies using interface.

--- empWage.java
+++ empWage.java
@@ -1,55 +1,63 @@
-class companyEmpWage
+interface companyEmployeewage
+{
+        public void addCompanyEmpWage(String company,int wage_per_hr, int workday_in_months,int max_working_hrs);
+        public void computeEmpWage();
+} //created interface and it means all the functions are abstract but we don't
+//have to mention it, thats why we create interface and implement it.
+
+class companyEmpWage 
 {
         //instance variables
         public static final int isFullTime=1;
         public static final int isPartTime=2;
-	public String company;
-	public int wage_per_hr;
-	public int workday_in_months;
-	public int max_working_hrs;
+        public String company;
+        public int wage_per_hr;
+        public int workday_in_months;
+        public int max_working_hrs;
 
         //parameterized constructor
         public companyEmpWage(String company, int wage_per_hr, int workday_in_months, int max_working_hrs)
         {
-		this.company= company;
-		this.wage_per_hr= wage_per_hr;
-		this.workday_in_months= workday_in_months;
-		this.max_working_hrs= max_working_hrs;
-	}
-}	
-public class empWage
+                this.company= company;
+                this.wage_per_hr= wage_per_hr;
+                this.workday_in_months= workday_in_months;
+                this.max_working_hrs= max_working_hrs;
+        }
+}
+//implementing interface
+public class empWage implements companyEmployeewage
 {
-	public static final int isFullTime=1;
+        public static final int isFullTime=1;
         public static final int isPartTime=2;
-	private int num_of_company=0;// private type variable
-	private companyEmpWage[] companyEmpWageArray;//private array
-	
-	public empWage()//default construcor
-	{
-		companyEmpWageArray=new companyEmpWage[4];
-	}
-	//Adding companies in arrays
-	private void addCompanyEmpWage(String company,int wage_per_hr, int workday_in_months,int max_working_hrs)
-	{
-		companyEmpWageArray[num_of_company]=new companyEmpWage(company,wage_per_hr,workday_in_months,max_working_hrs);
-		num_of_company++;
-	}
-	//calling computewage function and calculating empwage for every index
-	public void computeEmpWage()
-	{
-		for(int i=0;i<num_of_company;i++)
-		{
-			computeWage(companyEmpWageArray[i]);
-		}
-	}
+        private int num_of_company=0;// private type variable
+        private companyEmpWage[] companyEmpWageArray;//private array
 
-	//function to calculate salary for  employee
-	public void computeWage(companyEmpWage emp)
-	{
-		//local variables
-		int empHrs=0;
-		int totalempHrs=0;
-		int totalworkingdays=0;
+        public empWage()//default construcor
+        {
+                companyEmpWageArray=new companyEmpWage[4];
+        }
+        //Adding companies in arrays
+        public void addCompanyEmpWage(String company,int wage_per_hr, int workday_in_months,int max_working_hrs)
+        {
+                companyEmpWageArray[num_of_company]=new companyEmpWage(company,wage_per_hr,workday_in_months,max_working_hrs);
+                num_of_company++;
+        }
+        //calling computewage function and calculating empwage for every index
+        public void computeEmpWage()
+        {
+                for(int i=0;i<num_of_company;i++)
+                {
+                        computeWage(companyEmpWageArray[i]);
+                }
+        }
+
+        //function to calculate salary for  employee
+        public void computeWage(companyEmpWage emp)
+        {
+                //local variables
+                int empHrs=0;
+                int totalempHrs=0;
+                int totalworkingdays=0;
 
                 //loop will run untill it reaches 100 hours or 20 days
                 while(totalempHrs<=emp.max_working_hrs && totalworkingdays<=emp.workday_in_months)
@@ -73,8 +81,8 @@ public class empWage
                         totalworkingdays++; //incrementing while loop.
                 }
                         int TotalSalary= totalempHrs*emp.wage_per_hr; //Calculating total salary of employee
-		
-			//printing totalsalary of different companies employee
+
+                        //printing totalsalary of different companies employee
                         System.out.println( "Total Employee Salary of " + emp.company +" is " + TotalSalary);
         }
 
@@ -82,11 +90,12 @@ public class empWage
         public static void main(String[] args)
         {
                 System.out.println("Welcome to Employee Wage Computation Program in Main branch");
-		empWage ob=new empWage();//created object for empWage class
+                empWage ob=new empWage();//created object for empWage class
                 ob.addCompanyEmpWage("DeSaw",50,35,25);//calling funtion with the object
                 ob.addCompanyEmpWage("TCS",40,25,18);//calling function with the object
                 ob.addCompanyEmpWage("NetScope",50,20,15);//doing same like above two
-		ob.computeEmpWage();//calling fucntion with the object
+                ob.computeEmpWage();//calling fucntion with the object
+        }
 }
-}
+
 
