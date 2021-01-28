9109fb224fa2acc6a584ba78ad8a277b680d4cc9
SaurabhT311 <38470650+SaurabhT311@users.noreply.github.com>
2021-01-28 18:51:40 +0530

    Refractor[Saurabh] CalculatingWagesByGivenConditions

--- empWage.java
+++ empWage.java
@@ -1,18 +1,18 @@
 class empWage
 {
-	public static final int isFullTime=1;
-	public static final int isPartTime=2;  
-	public static final int max_working_Hrs=100;
-        public static final int workday_in_months=20;
-	public static final int wage_per_hr=20;	
+	public static final int IS_Full_Time=1;
+	public static final int IS_Part_Time=2;  
+	public static final int MAX_WORKING_HRS=100;
+        public static final int WORKDAY_IN_MONTHS=20;
+	public static final int WAGE_PER_HR=20;	
 
-        public static void empsalary()
+        public static void empSalary()
         {
 		int empHrs=0;
-		int totalworkingdays=0;
-		int totalempHrs=0;
+		int totalWorkingDays=0;
+		int totalEmpHrs=0;
 
-		while(totalempHrs<=max_working_Hrs && totalworkingdays<=workday_in_months)
+		while(totalEmpHrs<=MAX_WORKING_HRS && totalWorkingDays<=WORKDAY_IN_MONTHS)
 		{
 			int empCheck=(int)(Math.floor(Math.random()*10)%3);
 			switch(empCheck)
@@ -27,15 +27,15 @@ class empWage
 					empHrs=0;
 					break;
 			}
-			totalempHrs=totalempHrs+empHrs;
-			totalworkingdays++;
+			totalEmpHrs=totalEmpHrs+empHrs;
+			totalWorkingDays++;
 		}
-			int TotalSalary= totalempHrs*wage_per_hr;
+			int TotalSalary= totalEmpHrs*WAGE_PER_HR;
 			System.out.println("Total Employee Salary is: "+ TotalSalary);
 	}
 	public static void main(String[] args)
 	{
 		System.out.println("Welcome To Compute Wage Problrem");
-		empsalary();
+		empSalary();
         }
 }
